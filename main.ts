47143f1d6d6259e74d6b94abfa50252b4dcd6768
maths <130728002+aslskks@users.noreply.github.com>
2026-03-08 10:51:48 -0600

    ss

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
-enum RadioMessage {
-    message1 = 49434
-}
 radio.onReceivedNumber(function (n) {
+    if (n == PING) {
+        radio.sendNumber(PONG)
+    }
+    if (n == PONG) {
+        canalEncontrado = true
+        basic.showIcon(IconNames.Yes)
+    }
     if (n == PUNTO) {
         punto()
     } else if (n == RAYA) {
         raya()
     } else if (n == ESPACIO) {
         espacio()
-    } else if (n == Finished) {
+    } else if (n == FINISHED) {
         finished()
     }
 })
@@ -19,7 +23,7 @@ function finished () {
     basic.clearScreen()
 }
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    radio.sendNumber(Finished)
+    radio.sendNumber(FINISHED)
     finished()
 })
 input.onButtonPressed(Button.A, function () {
@@ -38,6 +42,9 @@ function raya () {
     basic.pause(50)
     basic.clearScreen()
 }
+input.onGesture(Gesture.ScreenDown, function () {
+    buscarCanal()
+})
 function punto () {
     basic.showLeds(`
         . . . . .
@@ -58,18 +65,41 @@ input.onButtonPressed(Button.B, function () {
     radio.sendNumber(RAYA)
     raya()
 })
+function buscarCanal () {
+    canalEncontrado = false
+    for (let i = 0; i <= 254; i++) {
+        if (canalEncontrado) {
+            break;
+        }
+        canalActual = i
+        radio.setGroup(i)
+        radio.sendNumber(PING)
+        basic.showNumber(i)
+    }
+    if (!(canalEncontrado)) {
+        basic.showIcon(IconNames.No)
+        basic.pause(100)
+        basic.clearScreen()
+        radio.setGroup(1)
+    }
+}
 function espacio () {
     basic.showIcon(IconNames.Diamond)
-    music.play(music.tonePlayable(880, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.playTone(880, 50)
     basic.pause(50)
     basic.clearScreen()
 }
-let Finished = 0
+let canalActual = 0
+let canalEncontrado = false
+let PONG = 0
+let PING = 0
+let FINISHED = 0
 let ESPACIO = 0
 let PUNTO = 0
 let RAYA = 0
 RAYA = 1
 PUNTO = 2
 ESPACIO = 3
-Finished = 4
-radio.setGroup(67)
+FINISHED = 4
+PING = 10
+PONG = 11
